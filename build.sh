621c12da1b0660bdbc9ff3702eff7805aebf2a91
Patrick Haggerty <hagg@haggconsulting.com>
2024-05-27 15:01:44 -0500

    Update for v1

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 PROJECT_ID="${PROJECT_ID:-$(gcloud config get-value project)}"
+LOCATION="us"
+REPOSITORY="cooking-images"
+FULL_REPO="${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY}"
+IMAGE_NAME="recipe-web-app"
 
-gcloud builds submit --tag us-docker.pkg.dev/$PROJECT_ID/cooking-images/recipe-web-app:latest .
+if ! gcloud artifacts repositories describe "${REPOSITORY}" --location="${LOCATION}" --project="${PROJECT_ID}" &>/dev/null; then
+    echo "Repository '$REPOSITORY' does not exist. Creating..."
 
-gcloud run deploy recipe-web-app --image=us-docker.pkg.dev/$PROJECT_ID/cooking-images/recipe-web-app:latest --region=us-central1 --port=8501
+    gcloud artifacts repositories create "${REPOSITORY}" \
+        --repository-format=docker \
+        --location="${LOCATION}" \
+        --project="${PROJECT_ID}"
+    echo "Repository '$FULL_REPO' created successfully."
+else
+    echo "Repository '$FULL_REPO' already exists."
+fi
+
+gcloud builds submit --tag $FULL_REPO/$IMAGE_NAME:latest .
+
+gcloud run deploy $IMAGE_NAME \
+    --platform=managed \
+    --allow-unauthenticated \
+    --image=$FULL_REPO/$IMAGE_NAME:latest --region=us-central1 --port=8501
