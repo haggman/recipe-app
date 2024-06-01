25f303f13359969f93e56fc245002bc65993f7ab
Patrick Haggerty <hagg@haggconsulting.com>
2024-06-01 10:52:35 -0500

    Remove search config

--- pages/Recipe_Search.py
+++ pages/Recipe_Search.py
@@ -42,32 +42,12 @@ def search_sample(
         serving_config="default_config",
     )
 
-    # Optional: Configuration options for search
-    # Refer to the `ContentSearchSpec` reference for all supported fields:
-    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest.ContentSearchSpec
-    content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
-        # For information about snippets, refer to:
-        # https://cloud.google.com/generative-ai-app-builder/docs/snippets
-        snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
-            return_snippet=True
-        ),
-        # For information about search summaries, refer to:
-        # https://cloud.google.com/generative-ai-app-builder/docs/get-search-summaries
-        summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
-            summary_result_count=5,
-            include_citations=True,
-            ignore_adversarial_query=True,
-            ignore_non_summary_seeking_query=True,
-        ),
-    )
-
     # Refer to the `SearchRequest` reference for all supported fields:
     # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest
     request = discoveryengine.SearchRequest(
         serving_config=serving_config,
         query=search_query,
         page_size=5,
-        content_search_spec=content_search_spec,
         query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
             condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO,
         ),
